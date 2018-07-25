4359c11c12ba1d281491dca3473f78238c4237a0
jigkoh3 <jigkoh3@hotmail.com>
2018-07-25 08:08:06 +0700

    [edit] test get & get by ID

--- src/modules/api/test/routes.js
+++ src/modules/api/test/routes.js
@@ -13,11 +13,11 @@ var credentials,
     token,
     mockup;
 
-describe(_model + ' CRUD routes tests', function () {
+describe(_model + ' CRUD routes tests', () => {
 
-    before(function (done) {
+    before((done) => {
         mockup = {
-            name : 'test'
+            name: 'test'
         };
         credentials = {
             username: 'username',
@@ -32,13 +32,50 @@ describe(_model + ' CRUD routes tests', function () {
         done();
     });
 
-    it('should be ' + _model + ' post use token', (done)=>{
+    it('should be ' + _model + ' get use token', (done)=>{
+        request(app)
+        .get('/api/' + _model)
+        .set('Authorization', 'Bearer ' + token)
+        .expect(200)
+        .end(done);
+    });
+
+    it('should be ' + _model + ' get by id use token', (done)=> {
+
         request(app)
             .post('/api/' + _model)
             .set('Authorization', 'Bearer ' + token)
             .send(mockup)
             .expect(200)
-            .end(function (err, res) {
+            .end((err, res)=> {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .get('/api/' + _model + '/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .expect(200)
+                    .end( (err, res)=> {
+                        if (err) {
+                            return done(err);
+                        }
+                        var resp = res.body;
+                        assert.equal(resp.status, 200);
+                        assert.equal(resp.data.name, mockup.name);
+                        done();
+                    });
+            });
+
+    });
+
+    it('should be ' + _model + ' post use token', (done) => {
+        request(app)
+            .post('/api/' + _model)
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end((err, res) => {
                 if (err) {
                     return done(err);
                 }
@@ -48,14 +85,14 @@ describe(_model + ' CRUD routes tests', function () {
             });
     });
 
-    it('should be ' + _model + ' put use token', function (done) {
+    it('should be ' + _model + ' put use token', (done) => {
 
         request(app)
             .post('/api/' + _model)
             .set('Authorization', 'Bearer ' + token)
             .send(mockup)
             .expect(200)
-            .end(function (err, res) {
+            .end((err, res) => {
                 if (err) {
                     return done(err);
                 }
@@ -68,7 +105,7 @@ describe(_model + ' CRUD routes tests', function () {
                     .set('Authorization', 'Bearer ' + token)
                     .send(update)
                     .expect(200)
-                    .end(function (err, res) {
+                    .end((err, res) => {
                         if (err) {
                             return done(err);
                         }
@@ -80,14 +117,14 @@ describe(_model + ' CRUD routes tests', function () {
 
     });
 
-    it('should be ' + _model + ' delete use token', function (done) {
+    it('should be ' + _model + ' delete use token', (done) => {
 
         request(app)
             .post('/api/' + _model)
             .set('Authorization', 'Bearer ' + token)
             .send(mockup)
             .expect(200)
-            .end(function (err, res) {
+            .end((err, res) => {
                 if (err) {
                     return done(err);
                 }
@@ -101,7 +138,34 @@ describe(_model + ' CRUD routes tests', function () {
 
     });
 
-    it('should be ' + _model + ' post not use token', function (done) {
+    it('should be ' + _model + ' get not use token', (done)=>{
+        request(app)
+        .get('/api/' + _model)
+        .expect(403)
+        .end(done);
+    });
+
+    it('should be ' + _model + ' get by id not use token', (done)=> {
+
+        request(app)
+            .post('/api/' + _model)
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end((err, res)=> {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .get('/api/' + _model + '/' + resp.data._id)
+                    .expect(403)
+                    .end(done);
+            });
+
+    });
+
+    it('should be ' + _model + ' post not use token', (done) => {
 
         request(app)
             .post('/api/' + _model)
@@ -115,14 +179,14 @@ describe(_model + ' CRUD routes tests', function () {
 
     });
 
-    it('should be ' + _model + ' put not use token', function (done) {
+    it('should be ' + _model + ' put not use token', (done) => {
 
         request(app)
             .post('/api/' + _model)
             .set('Authorization', 'Bearer ' + token)
             .send(mockup)
             .expect(200)
-            .end(function (err, res) {
+            .end((err, res) => {
                 if (err) {
                     return done(err);
                 }
@@ -143,14 +207,14 @@ describe(_model + ' CRUD routes tests', function () {
 
     });
 
-    it('should be ' + _model + ' delete not use token', function (done) {
+    it('should be ' + _model + ' delete not use token', (done) => {
 
         request(app)
             .post('/api/' + _model)
             .set('Authorization', 'Bearer ' + token)
             .send(mockup)
             .expect(200)
-            .end(function (err, res) {
+            .end((err, res) => {
                 if (err) {
                     return done(err);
                 }
